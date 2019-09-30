9aa54070d69061027bc33cd9a83f42898552778b
Alex Weh <alex.jan.w@gmail.com>
2019-09-30 15:54:10 +0200

    Fixed Player generation

--- force-app/main/default/lwc/keySetter/keySetter.js
+++ force-app/main/default/lwc/keySetter/keySetter.js
@@ -1,10 +1,13 @@
 import { LightningElement } from 'lwc';
 import ID from '@salesforce/user/Id';
 import API_KEY from '@salesforce/schema/User.API_Key__c';
+import TIME_GENERATED from '@salesforce/schema/User.Time_Key_Generated__c';
 
 export default class KeySetter extends LightningElement {
     
     userId = ID;
     apiKey = API_KEY;
+    timeGenerated = TIME_GENERATED;
+    //timeExpire = TIME_GENERATED + 86400000;
     user = "User";
 }
