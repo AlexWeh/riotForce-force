6647d526fd083326b353557ab9102d35a82a6864
Alex Weh <alex.jan.w@gmail.com>
2019-09-30 13:45:41 +0200

    added keySetter lwc

--- force-app/main/default/lwc/matchIDRequester/matchIDRequester.js
+++ force-app/main/default/lwc/matchIDRequester/matchIDRequester.js
@@ -1,8 +1,34 @@
-import { LightningElement } from 'lwc';
-import makeRequest from '@salesforce/apex/RiotMatchIdRequester.makeRequest';
+/* eslint-disable no-console */
 
+import { LightningElement, track } from 'lwc';
+import makeRequest from '@salesforce/apex/RiotMatchIdRequester.makeRequest';
 export default class MatchIDRequester extends LightningElement {
-    handleRequest(event){
-        makeRequest(event);
+    @track result;
+    @track matchId = '';
+    key = 'RGAPI-e12a538a-51e7-446f-9569-c523b4034f3e';
+    
+
+    handleMatchId(event){
+        this.matchId = event.target.value;
+    }
+
+    handleKey(event){
+        this.key = event.target.value;
+    }
+
+    handleRequest(){
+        console.log('Fire!');
+        console.log(this.matchId);
+        makeRequest({ matchId: this.matchId, key: this.key })
+            .then(result => {
+                this.result = result;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.result = undefined;
+            });
+        console.log(this.result);
+        console.log(this.error);
     }
 }
