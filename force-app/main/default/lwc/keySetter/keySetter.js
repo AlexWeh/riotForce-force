2f8efd5b4b8672dc13b0ecfd3a444658a5db14bd
Alex Weh <alex.jan.w@gmail.com>
2019-09-30 14:33:23 +0200

    extracted the API Key. Can easily be changed now

--- force-app/main/default/lwc/keySetter/keySetter.js
+++ force-app/main/default/lwc/keySetter/keySetter.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import ID from '@salesforce/user/Id';
+import API_KEY from '@salesforce/schema/User.API_Key__c';
 
-export default class KeySetter extends LightningElement {}
+export default class KeySetter extends LightningElement {
+    
+    userId = ID;
+    apiKey = API_KEY;
+    user = "User";
+}
--- force-app/main/default/lwc/matchIDRequester/matchIDRequester.js
+++ force-app/main/default/lwc/matchIDRequester/matchIDRequester.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-
 import { LightningElement, track } from 'lwc';
+import ID from '@salesforce/user/Id';
 import makeRequest from '@salesforce/apex/RiotMatchIdRequester.makeRequest';
+
 export default class MatchIDRequester extends LightningElement {
+    userID = ID;
     @track result;
     @track matchId = '';
-    key = 'RGAPI-e12a538a-51e7-446f-9569-c523b4034f3e';
-    
 
     handleMatchId(event){
         this.matchId = event.target.value;
@@ -18,8 +18,8 @@ export default class MatchIDRequester extends LightningElement {
 
     handleRequest(){
         console.log('Fire!');
-        console.log(this.matchId);
-        makeRequest({ matchId: this.matchId, key: this.key })
+        console.log(this.userID);
+        makeRequest({ matchId: this.matchId, userID: this.userID })
             .then(result => {
                 this.result = result;
                 this.error = undefined;
